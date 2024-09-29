a08040fa0ef4c7cddd417a6578d3abb6a32d2072
Diego Reyes <diegorele13@gmail.com>
2024-09-28 23:36:37 -0500

    dad: dfa

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,15 @@ import HomeView from './views/HomeView.vue';
 
 <style scoped>
 .principal {
-  background-color: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
   height: 100vh;
+  background-repeat: no-repeat;
+  background-size: cover; /* Asegura que la imagen cubra todo el fondo */
+  background-position: center; /* Centra la imagen */
+  background-image: url("https://i.pinimg.com/564x/e3/5c/5d/e35c5d22fb28d06569f2a8871b272740.jpg");
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from 'vue'
 
 const poemasTraducidos = ref([])
+const mostrarPoema = ref(false)
 
 async function obtenerYTraducirPoemas() {
   try {
@@ -32,19 +33,31 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container p-4"> 
-    <h1 class="text-center mb-4">Poema de esta vez ❤️</h1>
-    <div v-for="(poema, index) in poemasTraducidos" :key="index" class="poema card p-4 shadow-lg mb-4">
-      <h3 class="card-title text-center display-4">{{ poema.titulo }}</h3>
-      <h3 class="card-subtitle text-muted text-center mb-3">{{ poema.autor }}</h3>
-      <p class="card-text lead">{{ poema.texto }}</p>
-    </div>
+  <div class="container p-4 d-flex justify-content-center align-items-center flex-column"> 
+    <h1 class="text-center mb-4 text-white">Poema de esta vez ❤️ para Nadia el amor de mi vida, razón de mi existir</h1>
+    <button @click="mostrarPoema = !mostrarPoema" class="btn btn-danger mb-4">{{ mostrarPoema ? 'Ocultar poema' : 'Mostrar poema' }}</button> 
+    <transition name="fade">
+      <div v-if="mostrarPoema" class="poema card p-4 shadow-lg mb-4 bg-light"> 
+        <h3 class="card-title text-center display-4 font-italic">{{ poemasTraducidos[0].titulo }}</h3>
+        <h3 class="card-subtitle text-muted text-center mb-3 font-weight-light">{{ poemasTraducidos[0].autor }}</h3>
+        <p class="card-text lead text-justify">{{ poemasTraducidos[0].texto }}</p>
+      </div>
+    </transition>
   </div>
-</template >
+</template>
 
 
 <style scoped>
 .poema {
   margin-bottom: 20px;
 }
+
+/* Transiciones */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s, background-color 0.5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
+
